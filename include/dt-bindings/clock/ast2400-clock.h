ab1375f7db7ad9b9e73fbc7c67c0e10f411a7b0d
ryan_chen <ryan_chen@aspeedtech.com>
2020-07-21 16:29:25 +0800

    add ast2400 clk

--- include/dt-bindings/clock/ast2400-clock.h
+++ include/dt-bindings/clock/ast2400-clock.h
@@ -12,34 +12,34 @@
 #define ASPEED_CLK_GATE_USBPORT2CLK     7
 #define ASPEED_CLK_GATE_LCLK            8
 #define ASPEED_CLK_GATE_USBUHCICLK      9
-#define ASPEED_CLK_GATE_D1CLK           10
-#define ASPEED_CLK_GATE_YCLK            11
-#define ASPEED_CLK_GATE_USBPORT1CLK     12
-#define ASPEED_CLK_GATE_UART1CLK        13
-#define ASPEED_CLK_GATE_UART2CLK        14
-#define ASPEED_CLK_GATE_UART5CLK        15
-#define ASPEED_CLK_GATE_ESPICLK         16
-#define ASPEED_CLK_GATE_MAC1CLK         17
-#define ASPEED_CLK_GATE_MAC2CLK         18
-#define ASPEED_CLK_GATE_RSACLK          19
-#define ASPEED_CLK_GATE_UART3CLK        20
-#define ASPEED_CLK_GATE_UART4CLK        21
-#define ASPEED_CLK_GATE_SDCLK           22
-#define ASPEED_CLK_GATE_LHCCLK          23
-#define ASPEED_CLK_GATE_SDEXTCLK        24
-#define ASPEED_CLK_HPLL                 25
-#define ASPEED_CLK_AHB                  26
-#define ASPEED_CLK_APB                  27
-#define ASPEED_CLK_UART                 28
-#define ASPEED_CLK_SDIO                 29
-#define ASPEED_CLK_ECLK                 30
-#define ASPEED_CLK_ECLK_MUX             31
-#define ASPEED_CLK_LHCLK                32
-#define ASPEED_CLK_MAC                  33
-#define ASPEED_CLK_BCLK                 34
-#define ASPEED_CLK_MPLL                 35
-#define ASPEED_CLK_24M                  36
-
-#define ASPEED_CLK_DPLL                 37
-#define ASPEED_CLK_D2PLL                 38
+#define ASPEED_CLK_GATE_D2CLK           10
+/* 11 12 reserved */
+#define ASPEED_CLK_GATE_YCLK            13
+#define ASPEED_CLK_GATE_USBPORT1CLK     14
+#define ASPEED_CLK_GATE_UART1CLK        15
+#define ASPEED_CLK_GATE_UART2CLK        16
+#define ASPEED_CLK_GATE_UART5CLK        17
+/* 18 19 reserved */
+#define ASPEED_CLK_GATE_MAC1CLK         20
+#define ASPEED_CLK_GATE_MAC2CLK         21
+/* 22 23 reserved */
+#define ASPEED_CLK_GATE_RSACLK          24
+#define ASPEED_CLK_GATE_UART3CLK        25
+#define ASPEED_CLK_GATE_UART4CLK        26
+#define ASPEED_CLK_GATE_SDCLK           27
+#define ASPEED_CLK_GATE_LHCCLK          28
+#define ASPEED_CLK_GATE_SDEXTCLK        29
+#define ASPEED_CLK_HPLL                 30
+#define ASPEED_CLK_AHB                  31
+#define ASPEED_CLK_APB                  32
+#define ASPEED_CLK_UART                 33
+#define ASPEED_CLK_SDIO                 34
+#define ASPEED_CLK_ECLK                 35
+#define ASPEED_CLK_ECLK_MUX             36
+#define ASPEED_CLK_LHCLK                37
+#define ASPEED_CLK_MAC                  38
+#define ASPEED_CLK_BCLK                 39
+#define ASPEED_CLK_MPLL                 40
+#define ASPEED_CLK_24M                  41
+#define ASPEED_CLK_D2PLL                42
 
