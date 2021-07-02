a07430c08de2df5924a3cf88d26fd2e5c7968b6b
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2021-07-02 13:46:46 +0800

    configs: intel_ast2600: Add SPL FIT load support
    1. Add load address for SPL FIT image load support
    2. Revise boot command to copy kernel FIT image into DRAM for
       HW crypto engine to perform hash digest and signature verification
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: I37cf56a945406464467f40b682f06eef37593772

--- include/configs/intel_ast2600.h
+++ include/configs/intel_ast2600.h
@@ -17,19 +17,23 @@
 /* Memory Info */
 #define CONFIG_SYS_LOAD_ADDR		0x83000000
 
-#ifdef CONFIG_SPL_TINY
-#ifdef CONFIG_SPL_BUILD
-#define CONFIG_SYS_NS16550_REG_SIZE 2
-#endif
-#endif
-
 /* SPL */
 #define CONFIG_SPL_TEXT_BASE		0x00000000
 #define CONFIG_SPL_MAX_SIZE		0x00010000
 #define CONFIG_SPL_STACK		0x10016000
 #define CONFIG_SPL_BSS_START_ADDR	0x90000000
 #define CONFIG_SPL_BSS_MAX_SIZE		0x00100000
+#define CONFIG_SPL_LOAD_FIT_ADDRESS	0x00010000
 
-#define CONFIG_SUPPORT_EMMC_BOOT
+/* Extra ENV for Boot Command */
+#define STR_HELPER(n)	#n
+#define STR(n)		STR_HELPER(n)
+
+#undef CONFIG_EXTRA_ENV_SETTINGS
+#define CONFIG_EXTRA_ENV_SETTINGS	\
+	"loadaddr=" STR(CONFIG_SYS_LOAD_ADDR) "\0"	\
+	"bootspi=fdt addr 20100000 && fdt header get fitsize totalsize && cp.b 20100000 ${loadaddr} ${fitsize} && bootm; echo Error loading kernel FIT image\0"	\
+	"verify=yes\0"	\
+	""
 
 #endif	/* __CONFIG_H */
