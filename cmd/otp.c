b63af886c84a32a1be311e9f2a19e5254ea0c6c8
Johnny Huang <johnny_huang@aspeedtech.com>
2021-07-29 19:47:25 +0800

    otp: Add AST2600A3 otp config
    AST2600A3 otp config is slightly different from A2, separate it to a
    dedicated config.
    
    Signed-off-by: Johnny Huang <johnny_huang@aspeedtech.com>
    Change-Id: I448aa50c3f67993cef31da9ace0864f1ab0fd6c2

--- cmd/otp.c
+++ cmd/otp.c
@@ -2385,10 +2385,10 @@ static int do_ast_otp(cmd_tbl_t *cmdtp, int flag, int argc, char *const argv[])
 		break;
 	case OTP_A3:
 		info_cb.version = OTP_A3;
-		info_cb.conf_info = a2_conf_info;
-		info_cb.conf_info_len = ARRAY_SIZE(a2_conf_info);
-		info_cb.strap_info = a2_strap_info;
-		info_cb.strap_info_len = ARRAY_SIZE(a2_strap_info);
+		info_cb.conf_info = a3_conf_info;
+		info_cb.conf_info_len = ARRAY_SIZE(a3_conf_info);
+		info_cb.strap_info = a3_strap_info;
+		info_cb.strap_info_len = ARRAY_SIZE(a3_strap_info);
 		info_cb.key_info = a3_key_type;
 		info_cb.key_info_len = ARRAY_SIZE(a3_key_type);
 		sprintf(info_cb.ver_name, "A3");
