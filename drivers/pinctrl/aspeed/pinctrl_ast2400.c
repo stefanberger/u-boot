094f600371f9c3271bc21759c00cd3bd97d31472
Zev Weiss <zev@bewilderbeest.net>
2021-12-13 11:47:03 -0800

    pinctrl: ast2400: add support for TXD3/RXD3 pins
    In order to support putting the u-boot console on UART3 of the
    ast2400, this commit adds support for setting bits 22 and 23 of SCU80
    to enable TXD3 and RXD3 on pins C14 and B14, respectively.
    
    Signed-off-by: Zev Weiss <zev@bewilderbeest.net>
    Link: https://lore.kernel.org/r/20211213194703.24060-1-zev@bewilderbeest.net
    Signed-off-by: Joel Stanley <joel@jms.id.au>

--- drivers/pinctrl/aspeed/pinctrl_ast2400.c
+++ drivers/pinctrl/aspeed/pinctrl_ast2400.c
@@ -125,6 +125,14 @@ static struct aspeed_sig_desc spi1_link[] = {
 	{ 0x70, BIT(12), 0},
 };
 
+static struct aspeed_sig_desc txd3_link[] = {
+	{ 0x80, BIT(22), 0},
+};
+
+static struct aspeed_sig_desc rxd3_link[] = {
+	{ 0x80, BIT(23), 0},
+};
+
 static const struct aspeed_group_config ast2400_groups[] = {
 	{ "MAC1LINK", 1, mac1_link },
 	{ "MAC2LINK", 1, mac2_link },
@@ -146,6 +154,8 @@ static const struct aspeed_group_config ast2400_groups[] = {
 	{ "SD1", 1, sdio1_link },
 	{ "SPI1", 1, spi1_link},
 	{ "SPI1CS1", 1, spi1cs1_link},
+	{ "TXD3", 1, txd3_link },
+	{ "RXD3", 1, rxd3_link },
 };
 
 static int ast2400_pinctrl_get_groups_count(struct udevice *dev)
