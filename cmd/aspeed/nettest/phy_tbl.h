ff6a54e274545914b5736158509cbd2374485cb9
Dylan Hung <dylan_hung@aspeedtech.com>
2022-04-01 13:39:12 +0800

    cmd: aspeed: nettest: add RTL8211F(D)-VD-CG support
    add new PHY Realtek RTL8211F(D)-VD-CG support.  This PHY is basically
    the same with RTL8211F series so use the same configuration for it.
    
    Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
    Change-Id: I38c971e2e94aafb59db895302eb5c3d44814dd65

--- cmd/aspeed/nettest/phy_tbl.h
+++ cmd/aspeed/nettest/phy_tbl.h
@@ -45,7 +45,7 @@ struct phy_desc {
 	PHY_ENGINE cfg;
 };
 
-#define PHY_LOOKUP_N 34
+#define PHY_LOOKUP_N 35
 static const struct phy_desc phy_lookup_tbl[PHY_LOOKUP_N] = {
 	{ .id1 = 0x001c,
 	  .id2 = 0xc916,
@@ -57,6 +57,12 @@ static const struct phy_desc phy_lookup_tbl[PHY_LOOKUP_N] = {
 	  .id2 = 0xc859,
 	  .id2_mask = 0xffff,
 	  .name = "RTL8211FD-VX",
+	  .cfg.fp_set = phy_realtek5,
+	  .cfg.fp_clr = recov_phy_realtek5 },
+	{ .id1 = 0x001c,
+	  .id2 = 0xc870,
+	  .id2_mask = 0xfff0,
+	  .name = "RTL8211F-VD",
 	  .cfg.fp_set = phy_realtek5,
 	  .cfg.fp_clr = recov_phy_realtek5 },
 	{ .id1 = 0x001c,
