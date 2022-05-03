b88f3710ee3880fa17cdfaa744f4c81e44f092ae
Chia-Wei Wang <chiawei_wang@aspeedtech.com>
2022-05-03 11:09:02 +0800

    arm: aspeed: ast2600: Remove ASPEED_LOADERS
    ASPEED_LOADERS was intended to support AST2600 proprietary CoT.
    Now the CoT is replaced by the U-Boot FIT image verification.
    
    Remove ASPEED_LOADERS to avoid using the legacy boot methods.
    The ASPEED_SECURE_BOOT used within ASPEED_LOADERS implementation
    is removed as well.
    
    Signed-off-by: Chia-Wei Wang <chiawei_wang@aspeedtech.com>
    Change-Id: Ie718ebffbb108d4a8bd504845b017b455d50faba

--- arch/arm/mach-aspeed/ast2600/spl.c
+++ arch/arm/mach-aspeed/ast2600/spl.c
@@ -50,21 +50,6 @@ void spl_board_init(void)
 
 u32 spl_boot_device(void)
 {
-#ifdef CONFIG_ASPEED_LOADERS
-	switch (aspeed_bootmode()) {
-	case AST_BOOTMODE_EMMC:
-		return (IS_ENABLED(CONFIG_ASPEED_SECURE_BOOT))?
-			ASPEED_SECBOOT_DEVICE_MMC : ASPEED_BOOT_DEVICE_MMC;
-	case AST_BOOTMODE_SPI:
-		return (IS_ENABLED(CONFIG_ASPEED_SECURE_BOOT))?
-			ASPEED_SECBOOT_DEVICE_RAM : ASPEED_BOOT_DEVICE_RAM;
-	case AST_BOOTMODE_UART:
-		return (IS_ENABLED(CONFIG_ASPEED_SECURE_BOOT))?
-			ASPEED_SECBOOT_DEVICE_UART : ASPEED_BOOT_DEVICE_UART;
-	default:
-		break;
-	}
-#else
 	switch (aspeed_bootmode()) {
 	case AST_BOOTMODE_EMMC:
 		return BOOT_DEVICE_MMC1;
@@ -75,7 +60,6 @@ u32 spl_boot_device(void)
 	default:
 		break;
 	}
-#endif
 
 	return BOOT_DEVICE_NONE;
 }
