stefanberger/u-boot

commit cb2cc580a14a3948f9c9d8e537a2a3336e4197d2

ram: ast2600: add option to configure DRAM output impedance
The DRAM output impedance is controlled by MR1[A2:A1].  Add an option to
make it configurable.

Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
Change-Id: I0f2c5e9bd196a2dbb81e5a8a588f33079340a7ae

drivers/ram/aspeed/sdram_phy_ast2600.h
@@ -38,6 +38,17 @@
 #endif
 
 /**
+ * MR1[2:1] output driver impedance
+ *   b'00 : 34 ohm (default)
+ *   b'01 : 48 ohm
+ */
+#ifdef ASPEED_DDR4_DRAM_RON_48
+#define DRAM_RON			(0x1 << 1)
+#else
+#define DRAM_RON			(0x0 << 1)
+#endif
+
+/**
  * phyr058[10:8] - ODT configuration (DRAM side)
  *   b'001 : 60 ohm
  *   b'101 : 48 ohm
@@ -76,7 +87,7 @@ u32 ast2600_sdramphy_config[165] = {
 	0x00003080,	// phyr04c
 	0x04000000,	// phyr050
 	0x00000200,	// phyr054
-	(0x03140001 | DRAM_ODT),	// phyr058
+	(0x03140001 | DRAM_ODT | DRAM_RON),	/* phyr058 */
 	0x04800000,	// phyr05c
 	0x0800044e,	// phyr060
 	0x00000000,	// phyr064
@@ -244,7 +255,7 @@ u32 ast2600_sdramphy_config[165] = {
 	0x00003080,	// phyr04c
 	0x04000000,	// phyr050
 	0x00000200,	// phyr054
-	(0x03140001 | DRAM_ODT),	// phyr058
+	(0x03140001 | DRAM_ODT | DRAM_RON),	/* phyr058 */
 	0x04800000,	// phyr05c
 	0x0800044e,	// phyr060
 	0x00000000,	// phyr064

drivers/ram/aspeed/Kconfig
@@ -115,4 +115,21 @@ config ASPEED_DDR4_DRAM_ODT40
 	help
 	  select DDR4 DRAM side ODT 40 ohm
 endchoice
+
+choice
+	prompt "DDR4 DRAM output driver impedance"
+	default ASPEED_DDR4_DRAM_RON_34
+
+config ASPEED_DDR4_DRAM_RON_34
+	bool "DDR4 DRAM output driver impedance 34 ohm"
+	depends on DM && OF_CONTROL && ARCH_ASPEED
+	help
+	  select DDR4 DRAM output driver impedance 34 ohm
+
+config ASPEED_DDR4_DRAM_RON_48
+	bool "DDR4 DRAM output driver impedance 48 ohm"
+	depends on DM && OF_CONTROL && ARCH_ASPEED
+	help
+	  select DDR4 DRAM output driver impedance 48 ohm
+endchoice
 endif

commit e2ef8286d561985453ef74ba867678126120a3d0

ram: ast2600: fix typo of CONFIG_ASPEED_DDR4_DRAM_RON_48
Fix the typo for missing "CONFIG_" of CONFIG_ASPEED_DDR4_DRAM_RON_48

Fixes: cb2cc580a14a ("ram: ast2600: add option to configure DRAM output impedance")

Signed-off-by: Dylan Hung <dylan_hung@aspeedtech.com>
Change-Id: I7cc4a9ac904e5562a8a5a4d7075b3cd5fe1f5810

drivers/ram/aspeed/sdram_phy_ast2600.h
@@ -42,7 +42,7 @@
  *   b'00 : 34 ohm (default)
  *   b'01 : 48 ohm
  */
-#ifdef ASPEED_DDR4_DRAM_RON_48
+#ifdef CONFIG_ASPEED_DDR4_DRAM_RON_48
 #define DRAM_RON			(0x1 << 1)
 #else
 #define DRAM_RON			(0x0 << 1)