c916075f81c7f43e04b437ea7c388f102d6ca87b
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2023-06-20 18:29:15 +0800

    mtd: spi-nor: Add gd55b01gf and gd55b02gf ID info
    Add gd55b01gf and gd55b02gf flash parts information.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I1672f50717b8ddd18f40cdfd91ef4ca3944404cd

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -122,6 +122,16 @@ const struct flash_info spi_nor_ids[] = {
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
 	{
+		INFO("gd55b01gf", 0xc8401b, 0, 64 * 1024, 2048,
+		     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+		     SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
+	},
+	{
+		INFO("gd55b02gf", 0xc8401c, 0, 64 * 1024, 4096,
+		     SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+		     SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
+	},
+	{
 		INFO("gd55b01ge", 0xc8471b, 0, 64 * 1024, 2048,
 			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
