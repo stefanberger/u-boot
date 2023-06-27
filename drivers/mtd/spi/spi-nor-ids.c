080c98023f2b3f26c7aa9eb84daf26714c4b5299
Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
2023-06-27 10:46:50 +0800

    spi-nor: Remove dual mode for GDxxBxxGE flash part
    Since GDxxBxxGE flash part doesn't support SPI dual mode,
    SPI_NOR_DUAL_READ property should be removed.
    
    Signed-off-by: Chin-Ting Kuo <chin-ting_kuo@aspeedtech.com>
    Change-Id: I7f296d972da7f27eef6ef34301db1466b12926e0

--- drivers/mtd/spi/spi-nor-ids.c
+++ drivers/mtd/spi/spi-nor-ids.c
@@ -133,12 +133,12 @@ const struct flash_info spi_nor_ids[] = {
 	},
 	{
 		INFO("gd55b01ge", 0xc8471b, 0, 64 * 1024, 2048,
-			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			SECT_4K | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
 	{
 		INFO("gd55b02ge", 0xc8471c, 0, 64 * 1024, 4096,
-			SECT_4K | SPI_NOR_DUAL_READ | SPI_NOR_QUAD_READ |
+			SECT_4K | SPI_NOR_QUAD_READ |
 			SPI_NOR_HAS_LOCK | SPI_NOR_HAS_TB)
 	},
 #endif
